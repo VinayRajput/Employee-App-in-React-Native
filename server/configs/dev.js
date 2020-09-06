a5cb39407c8453a9a600c8a84210ca051e3de149
Vinay Rajput <vinkrins@gmail.com>
2020-09-06 18:30:19 +0530

    Update dev.js

--- server/configs/dev.js
+++ server/configs/dev.js
@@ -1,4 +1,4 @@
 module.exports={
   "ENV":"dev",
-  "MONGOURI":"mongodb+srv://employee-app:employee-@pp@cluster0.kvgsc.mongodb.net/employeeApp?retryWrites=true&w=majority"
+  "MONGOURI":"mongodb+srv://employeeApp:employee--@pp@cluster0.kvgsc.mongodb.net/employeeApp?retryWrites=true&w=majority"
 }
